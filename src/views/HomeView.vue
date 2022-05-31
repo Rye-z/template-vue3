780936d240e77dd0ac2879d7282ce9e33d9eff40
rye-z <894544438@qq.com>
2022-05-31 21:50:11 +0800

    add: atomic-styles.scss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,24 +6,9 @@ const value = computed(() => {
 </script>
 
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
   <el-button type="primary" @click="name += 1">
-    type
+    click
   </el-button>
   {{ value }}
-  <div>test</div>
-  <div class="fw-500 fs-40">
-    <div class="fs-30">
-      test
-    </div>
-  </div>
-  <div class="test" />
 </template>
 
-<style lang="scss" scoped>
-.test {
-  @include card(100, 200);
-}
-</style>
